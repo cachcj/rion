e305c00f4f94b736b4111e73108c256aa4cc8bd1
Riffecs <riffecs@gmail.com>
2022-05-10 21:42:37 +0200

    Reversw

--- docs/compile.sh
+++
@@ -1,3 +0,0 @@
-pdflatex -synctex=1 -interaction=nonstopmode "Pflichtenheft.tex" 
-makeindex -s nomencl.ist -o Pflichtenheft.nls Pflichtenheft.nlo
-pdflatex -synctex=1 -interaction=nonstopmode "Pflichtenheft.tex" 
