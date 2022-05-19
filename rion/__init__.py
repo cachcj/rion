86ad408297b5f94ff7d205da2b89a530c4a8e4a5
Riffecs <riffecs@gmail.com>
2022-05-19 21:25:46 +0200

    Test colors

--- rion/__init__.py
+++ rion/__init__.py
@@ -4,10 +4,23 @@
 """
 import sys
 
+import numpy as np
+
+from . import errors
+from . import runner
+
 
 def handler() -> None:
     """
         Manages the CL arguments and distributes them to appropriate commands.
     """
     if sys.argv:
-        print("Hallo")
+        # Deletes the path and the basic command from the array
+        command_list = np.delete(np.array(sys.argv), [0, 1])
+
+        # Transfer the NumPy array with all configs to the relevant functions.
+        if sys.argv[1] == "install":
+            # Converts the Numpy array back to a normal list
+            runner.install(np.ndarray.tolis(command_list))
+    else:
+        errors.noinput()
---
+++ rion/runner.py
@@ -0,0 +1,8 @@
+"""
+    This is where the real magic happens
+"""
+
+def install(content: list) -> None:
+    """
+
+    """
