e519a9e09cc3d1700c9780dc6deb576b96ac5b20
Riffecs <102326693+Riffecs@users.noreply.github.com>
2022-05-21 16:36:36 +0200

    Delete test.py

--- rion/test.py
+++
@@ -1,7 +0,0 @@
-from rion import db
-
-db.create_database("rion")
-db.create_table("rion", "package", "name text, version text")
-db.input_value("rion", "package", "('Test', 'v1.0')")
-db.input_value("rion", "package", "('Tasdasfaasadd', 'v2.0')")
-db.list_table("rion", "package", "name")
