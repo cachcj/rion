177ff9ac2cc282b26ada90e41f4cd3cee6f26cd8
Riffecs <riffecs@gmail.com>
2022-05-30 22:30:28 +0200

    upload

--- rion/mktar.py
+++ rion/mktar.py
@@ -1,22 +1,37 @@
 """
     Make tar files
 """
-# pylint: disable = E, W, R, C
 
-import os.path
+import os
+import shutil
 import tarfile
 
+from rion import helper
 
-def make_tarfile(output_filename: str, source_dir: str) -> None:
+
+def make_tarfile(output_filename: str, source_dir: str) -> str:
     """
-        TODO: Make
+    Returns the path of the tarfile
     """
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
+    return helper.os_bindings(f"{os.getcwd()}/{output_filename}")
 
 
-def unmake_tarfile(output_filename: str, source_dir: str) -> None:
+def unmake_tarfile(input_filename: str, source_dir: str) -> None:
     """
-     TODO: Unmke
+    TODO: Unmke
     """
-    print(output_filename, source_dir)
+    os.mkdir(input_filename[:-3])
+    os.chdir(f"{os.getcwd()}/{input_filename}")
+    tar = tarfile.open(input_filename, "r:gz")
+    tar.extractall()
+    tar.close()
+    os.chdir("..")
+
+
+def move_tar(path_to_file: str, path_to_aim: str) -> bool:
+    """
+    Move the Tarfile
+    """
+    return shutil.move(path_to_file, path_to_aim)
