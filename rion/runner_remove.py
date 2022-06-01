95f1a3976165b5cb94c654a20747b95abb4bc7c1
cachcj <103600263+cachcj@users.noreply.github.com>
2022-06-01 16:27:11 +0200

    Update runner_remove.py

--- rion/runner_remove.py
+++ rion/runner_remove.py
@@ -22,8 +22,8 @@ def runnable_remove(db_name: str, table_name: str, pkg_name: str, content: str) 
         print(f"{content} cannot be found on your machine.")
 
     try:
-        db.delete_package(db_name, table_name, content)
-        print(f"{content} has been removed from the database.")
+        db.delete_package(db_name, table_name, pkg_name)
+        print(f"{pkg_name} has been removed from the database.")
 
     except sqlite3.Error as error:
         errors.sqlerror(error)
