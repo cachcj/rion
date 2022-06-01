bd685f8b5f67556e352b271c0c15999a51b0c655
cachcj <103600263+cachcj@users.noreply.github.com>
2022-06-01 14:53:37 +0200

    Update runner_remove.py

--- rion/runner_remove.py
+++ rion/runner_remove.py
@@ -7,23 +7,23 @@ import sqlite3
 from rion import db
 from rion import errors
 
-def runnable_remove(db_name: str, db_table: str, package: str) -> None:
+def runnable_remove(db_name: str, table_name: str, pkg_name: str, content: str) -> None:
     """
     Löscht Packete vom System und aus der Datenbank
     """
-    db_namex = package + db_name
+    db_namex = content + db_name
     print(db_namex)
 
     # If package exists on machine, delete it
-    if os.path.exists(package):
-        os.remove(package)
-        print(f"{package} has been removed from your machine.")
-    elif os.path.exists(package) is False:
-        print(f"{package} cannot be found on your machine.")
+    if os.path.exists(content):
+        os.remove(content)
+        print(f"{content} has been removed from your machine.")
+    elif os.path.exists(content) is False:
+        print(f"{content} cannot be found on your machine.")
 
     try:
-        db.delete_package(db_name, db_table, package)
-        print(f"{package} has been removed from the database.")
+        db.delete_package(db_name, db_table, content)
+        print(f"{content} has been removed from the database.")
 
     except sqlite3.Error as error:
         errors.sqlerror(error)
