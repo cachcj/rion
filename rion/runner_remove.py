cb8479621534db8881f4c7e9816cf3f3b5abda66
cachcj <103600263+cachcj@users.noreply.github.com>
2022-06-01 10:12:22 +0200

    Update runner_remove.py
    Called functions from db and errors when deleting package from database

--- rion/runner_remove.py
+++ rion/runner_remove.py
@@ -3,37 +3,45 @@ Delete Package
 """
 
 import os
-import sqlite3
+import sqlite
+from rion import db
+from rion import errors
 
-def runnable_remove(db_name: str, content: str) -> None:
+def runnable_remove(db_name: str, db_table: str, package: str) -> None:
     """
     Löscht Packete vom System und aus der Datenbank
     """
-    db_namex = content + db_name
+    db_namex = package + db_name
     print(db_namex)
 
     # If package exists on machine, delete it
-    if os.path.exists(content):
-        os.remove(content)
-        print(f"{content} has been removed from your machine.")
-    elif os.path.exists(content) is False:
-        print(f"{content} cannot be found on your machine.")
+    if os.path.exists(package):
+        os.remove(package)
+        print(f"{package} has been removed from your machine.")
+    elif os.path.exists(package) is False:
+        print(f"{package} cannot be found on your machine.")
 
-    # If package exists in database, delete it
+    ## If package exists in database, delete it
+    #try:
+        ## Connect to sqlite
+        #con = sqlite3.connect(f"{db_name}.db")
+            ## Create cursor object
+        #cur = con.cursor()
+
+        ## Deletes package from database
+        #cur.execute(f"DELETE FROM {db_table} WHERE {package}")
+        #con.execute.commit()
+        #print(f"{package} has been removed from the database.")
+    ## Show error if error
+    #except sqlite3.Error as error:
+        #print("There was an error: ", error)
+
+    #finally:
+        #con.close()
+        #cur = None
     try:
-        # Connect to sqlite
-        con = sqlite3.connect(f"{db_name}.db")
-            # Create cursor object
-        cur = con.cursor()
+        db.delete_package(db_name, db_table, package)
+        print(f"{package} has been removed from the database.")
 
-        # Deletes package from database
-        cur.execute(f"DELETE FROM {db_name} where content = {content}")
-        con.execute.commit()
-        print(f"{content} has been removed from the database.")
-    # Show error if error
     except sqlite3.Error as error:
-        print("There was an error: ", error)
-
-    finally:
-        con.close()
-        cur = None
+        errors.sqlerror(error)
