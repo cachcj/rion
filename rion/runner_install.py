8047b6ac0382ab441f4dc190ee2943962772cf31
Riffecs <riffecs@gmail.com>
2022-06-08 00:52:09 +0200

    Fix: PyLint for #7

--- rion/runner_install.py
+++ rion/runner_install.py
@@ -17,19 +17,20 @@ def runnable_install(content: str) -> None:
 
     # extract given archive
     pathstring: str = os.getcwd()
-    
+
     # We change the directory to the Venv folder to install the package there.
     os.chdir(f"{pathstring}/venv/{content[1]}")
-    
+
     # The files are transmitted as "tar.gz". These are now unzipped and saved.
     with tarfile.open(content[0], "r") as archive:
         archive.extractall()
-    
-    # Then we change back to the root folder of rion. Unfortunately Linux behaves a bit stupid there    
+
+    # Then we change back to the root folder of rion. Unfortunately Linux behaves a bit stupid there
     os.chdir(pathstring)
 
     # The version number is part of the package. Therefore it must be read out.
-    version = content[0][abs(content[0][::-1].find("v-") - len(content[0])) - 1 : len(content[0]) - 7 : 1].replace("_", ".")
+    pos = lambda docker: abs(docker[::-1].find("v-") - len(docker)) - 1
+    version = content[0][pos(content[0]) : len(content[0]) - 7 : 1].replace("_", ".")
 
     # After the attempted installation we add the entry to the Rion database
     db.input_value(db_name, "installed", f"({content[0]}, {version}, {content[1]})")
