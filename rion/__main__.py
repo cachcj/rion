ca46a51919a5e9461c5ebefeee079dcbee367815
Riffecs <riffecs@gmail.com>
2022-06-11 18:30:24 +0200

    Add: Simple Python Test

--- rion/__main__.py
+++ rion/__main__.py
@@ -4,6 +4,12 @@
 """
 from . import handler
 
+
+# test test function
+def calc_add(zahl1: int, zahl2: int) -> int:
+    return zahl1 + zahl2
+
+
 if __name__ == "__main__":
     # enable entry points
     handler()
---
+++ test/test.py
@@ -0,0 +1,3 @@
+from rion.__main__ import calc_add
+
+assert calc_add(1, 2) == 3, "Should be 3"
