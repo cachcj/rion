de0b1b1f45625e52bdbc50bec2b7c53af438aa34
Riffecs <riffecs@gmail.com>
2022-06-17 23:28:38 +0200

    Upload

--- rion/rion.py
+++ rion/rion.py
@@ -151,7 +151,7 @@ class Rion:
         # Reload Config
         self.user = __init__.read_config()
 
-    def remove(self, name: str, venv: str, version: str) -> None:
+    def remove(self) -> None:
         """
         Remove Package from venv
         """
@@ -194,7 +194,7 @@ class Rion:
             module_layer: str = str(module_layer)
             # We cut off everything useless from the original string,
             # so that only the package name remains.
-            runner_layer_runner: str = module_layer[2: module_layer.index(",")][:-1]
+            runner_layer_runner: str = module_layer[2 : module_layer.index(",")][:-1]
             # The case occurs when the name is exactly the same.
             # Upper and lower case is respected.
             if runner_layer_runner == self.content:
