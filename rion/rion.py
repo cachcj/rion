e1a6f0a6f19909f5171a863b22e56e0f42ab7954
Riffecs <riffecs@gmail.com>
2022-06-18 13:37:22 +0200

    Upload

--- rion/rion.py
+++ rion/rion.py
@@ -214,7 +214,7 @@ class Rion:
             module_layer: str = str(module_layer)
             # We cut off everything useless from the original string,
             # so that only the package name remains.
-            runner_layer_runner: str = module_layer[2 : module_layer.index(",")][:-1]
+            runner_layer_runner: str = module_layer[2: module_layer.index(",")][:-1]
             # The case occurs when the name is exactly the same.
             # Upper and lower case is respected.
             if runner_layer_runner == self.content:
@@ -274,8 +274,9 @@ class Rion:
                 if "port" in line:
                     self.error.error_message("Port Exist")
             # write conf
-            runner.write(f"server={ipaddres}")
-            runner.write(f"port={port}")
+            print(f"server={ipaddres}\nport={port}")
+            runner.write(str(f"server={ipaddres}"))
+            runner.write(str(f"port={port}"))
 
         # Reload Config
         self.user = Helper.read_config("server")
