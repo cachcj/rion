f6c8cbb577afd9b80737bbbce8a1266236d23ce8
Riffecs <102326693+Riffecs@users.noreply.github.com>
2022-06-18 11:48:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def def_requirements():
 
 setuptools.setup(
     name="rion",
-    version="0.0.16",
+    version="0.0.1",
     description="Package Manager CLI",
     entry_points={"console_scripts": ["rion=rion:handler"]},
     data_files=[("man/man1", ["man/rion.1"])],
