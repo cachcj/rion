6176f22dba50090e6d43a25bfb208288db949598
Riffecs <riffecs@gmail.com>
2022-06-19 16:48:09 +0200

    Fix: Version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def def_requirements():
 
 setuptools.setup(
     name="rion",
-    version="v0.1.7",
+    version="v0.1.8",
     description="Package Manager CLI",
     entry_points={"console_scripts": ["rion=rion:handler"]},
     data_files=[("man/man1", ["man/rion.1"])],
