fae94fc66795b80ffc4d632d0264bca6c0933297
Riffecs <riffecs@gmail.com>
2022-06-21 13:23:27 +0200

    Fix: Sudo

--- rion/rion.py
+++ rion/rion.py
@@ -114,7 +114,7 @@ class Rion:
         Rion installer
         """
         # Test Sudo
-        if not self.helper.testsudo():
+        if self.helper.testsudo():
             self.helper.error.error_message("Please dont use sudo")
         # To install Rion we go to the user directory.
         os.chdir(self.path_user)
@@ -256,7 +256,7 @@ class Rion:
             module_layer: str = str(module_layer)
             # We cut off everything useless from the original string,
             # so that only the package name remains.
-            runner_layer_runner: str = module_layer[2 : module_layer.index(",")][:-1]
+            runner_layer_runner: str = module_layer[2: module_layer.index(",")][:-1]
             # The case occurs when the name is exactly the same.
             # Upper and lower case is respected.
             if runner_layer_runner == self.content[0]:
