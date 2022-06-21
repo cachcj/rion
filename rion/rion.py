55d22ca452216ee156839b9e722bac0180ad709e
Riffecs <riffecs@gmail.com>
2022-06-21 14:49:14 +0200

    Modify: Links

--- rion/rion.py
+++ rion/rion.py
@@ -256,7 +256,7 @@ class Rion:
             module_layer: str = str(module_layer)
             # We cut off everything useless from the original string,
             # so that only the package name remains.
-            runner_layer_runner: str = module_layer[2: module_layer.index(",")][:-1]
+            runner_layer_runner: str = module_layer[2 : module_layer.index(",")][:-1]
             # The case occurs when the name is exactly the same.
             # Upper and lower case is respected.
             if runner_layer_runner == self.content[0]:
@@ -368,11 +368,23 @@ class Rion:
         os.chdir(self.helper.os_bindings("node"))
         # Execute Command
         if command == "create":
-            os.mkdir(self.helper.os_bindings(venv))
+            try:
+                os.mkdir(self.helper.os_bindings(venv))
+            except OSError as error:
+                self.helper.error.error_message(
+                    f"The Venv already exists\nError: {error}"
+                )
         elif command == "remove":
-            shutil.rmtree(venv)
+            try:
+                shutil.rmtree(venv)
+            except OSError as error:
+                self.helper.error.error_message(
+                    f"The Venv cannot delete.\nError:{error}"
+                )
+
         elif command == "list":
-            print(os.listdir("."))
+            for runner in os.listdir("."):
+                print(runner)
 
     def info(self) -> None:
         """
