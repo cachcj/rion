6c15a1bc9b2ce6c4995b6d5189297b7f057a5d70
Riffecs <riffecs@gmail.com>
2022-07-09 10:53:02 +0200

    Fix Version Info

--- rion/__init__.py
+++ rion/__init__.py
@@ -39,63 +39,37 @@ def handler() -> None:
         # Set Version
         print("Version: " + str(riox.__version__))
 
-        # Test Version
-        invalid_version = False
-        if loader == "uninstall":
+        if loader == "install":
+            riox.install()
+        elif loader == "update":
+            riox.update()
+        elif loader == "upgrade":
+            riox.upgrade()
+        elif loader == "version":
+            riox.version()
+        elif loader == "remove":
+            riox.remove()
+        elif loader == "search":
+            riox.search()
+        elif loader == "freeze":
+            riox.freeze()
+        elif loader == "check":
+            riox.check()
+        elif loader == "installer":
+            riox.installer()
+        elif loader == "uninstall":
             riox.uninstall()
-        try:
-            os.chdir(Path.home())
-            os.chdir("rion")
-            version_old = ""
-            with open("rion.conf", encoding="utf8") as docker:
-                for line in docker.readlines():
-                    if "version" in line:
-                        version_old = line.replace(" ", "").split("=")[1]
-                        print(
-                            f"Version old:{version_old}\n"
-                            f" Version New: {riox.__version__.replace(' ', '')}"
-                        )
-            if version_old != riox.__version__.replace(" ", ""):
-                invalid_version = True
-        except OSError:
-            time.sleep(1)
-        finally:
-            if invalid_version:
-                errorx.error_message("Invalid Version\nPlease install Rion again")
-            # Transfer the NumPy array with all configs to the relevant functions.
-            if loader == "install":
-                riox.install()
-            elif loader == "update":
-                riox.update()
-            elif loader == "upgrade":
-                riox.upgrade()
-            elif loader == "version":
-                riox.version()
-            elif loader == "remove":
-                riox.remove()
-            elif loader == "search":
-                riox.search()
-            elif loader == "freeze":
-                riox.freeze()
-            elif loader == "check":
-                riox.check()
-            elif loader == "installer":
-                riox.installer()
-            elif loader == "uninstall":
-                riox.uninstall()
-            elif loader == "login":
-                riox.login()
-            elif loader == "server":
-                riox.server()
-            elif loader == "venv":
-                riox.manage_venv()
-            elif loader == "info":
-                riox.info()
-            else:
-                # If no command was found, it aborts the program.
-                errorx.error_message("no command was found")
-    else:
-        errorx.error_message("No Input")
+        elif loader == "login":
+            riox.login()
+        elif loader == "server":
+            riox.server()
+        elif loader == "venv":
+            riox.manage_venv()
+        elif loader == "info":
+            riox.info()
+        else:
+            # If no command was found, it aborts the program.
+            errorx.error_message("no command was found")
 
     # End Time Managment
     diff = datetime.now() - start
