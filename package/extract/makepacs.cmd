0a33168813c4a2ead1161fc35707ccb9b7319e05
Riffecs <102326693+Riffecs@users.noreply.github.com>
2022-07-17 02:40:22 +0200

    Delete makepacs.cmd

--- package/extract/makepacs.cmd
+++
@@ -1,7 +0,0 @@
-tar czvf buddy-v100_0_3.tar.gz buddy/
-tar czvf fellpflege-v43_1_5.tar.gz
-tar czvf futternapf-v2_0_2.tar.gz futternapf/
-tar czvf katzenfutter-v44_0_3.tar.gz katzenfutter/
-tar czvf scotty-v42_0_2.tar.gz scotty/
-tar czvf tapwater-v2_1.tar.gz tapwater/
-tar czvf packages.tar.gz * 
