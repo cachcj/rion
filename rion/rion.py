f9d511c5ff6d2079ed7e9b76d52e2a765a9f13a8
Riffecs <riffecs@gmail.com>
2022-07-17 11:11:54 +0200

    328e1eb9-d570-4e02-beb4-c0ae508094e1

--- rion/rion.py
+++ rion/rion.py
@@ -173,7 +173,7 @@ class Rion:
             # Database Management
             self.rion.create_database()
             # Name, Version, Venv
-            self.rion.create_table("test", "(name text, version text, venv text)")
+            self.rion.create_table("test", "name text, version text, venv text")
             # Config Managment
             with open("rion.conf", "w", encoding="utf8") as docker:
                 docker.write("conf=rion\n")
