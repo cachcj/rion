e24ec711bd3fcc4f8312d2705c3e5744c8a29458
Riffecs <riffecs@gmail.com>
2022-07-17 03:32:50 +0200

    e2fff788-b1c3-49a1-9917-04e4318fa15e

--- rion/rion.py
+++ rion/rion.py
@@ -145,7 +145,7 @@ class Rion:
         print(f"Name: {content[0]}")
         print(f"Version: {version}")
         print(f"Venv: {venv}")
-        self.rion.input_value(self.identify,self.identify+", Hallo, Hallo, Hallo")
+        self.rion.input_value(f"{self.identify} (id, name, version, venv)","id, Hallo, Hallo, Hallo")
        # self.rion.input_value(
        #     self.identify,
        #     f"{content[0]}-v{version}, {content[0]}, {str(version)}, {venv}")
@@ -174,7 +174,7 @@ class Rion:
             self.rion.create_database()
             # Name, Version, Venv
             self.rion.create_table(
-                f"{self.table}", f"{self.identify} text, name text, version text, venv text"
+                f"{self.table}", "id text, name text, version text, venv text"
             )
             # Config Managment
             with open("rion.conf", "w", encoding="utf8") as docker:
